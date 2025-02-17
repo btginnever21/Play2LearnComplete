e133244b308e9acb541cd678f117649e957f73ce
Bradley Ginnever <31748731+btginnever@users.noreply.github.com>
2025-02-17 13:05:07 -0600

    updates

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # local apps
-    "games.apps.GamesConfig"
+    "games.apps.GamesConfig",
+    'users.apps.UsersConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -77,12 +79,15 @@ WSGI_APPLICATION = "play2learn.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'math games',
+        'USER': 'postgres',
+        'PASSWORD': 'Willowbean',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -101,6 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# AUTHENTICATION SETTINGS
+AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Internationalization
