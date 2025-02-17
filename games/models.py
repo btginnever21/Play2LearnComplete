061484d6c8191cca2217a0aba8c767e1502e4229
Bradley Ginnever <31748731+btginnever@users.noreply.github.com>
2025-02-17 14:40:29 -0600

    updates

--- games/models.py
+++ games/models.py
@@ -18,18 +18,3 @@ class GameScore(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
 
-class Review(models.Model):
-    Review = models.TextField(max_length=200)
-    Username = models.TextField(max_length=20, blank=True)
-    def get_absolute_url(self):
-        return reverse('joke', args=[self.slug])
-    
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def get_absolute_url(self):
-        return reverse('reviews:detail', args=[str(self.pk)])
-
-
-    def __str__(self):
-        return self.question
--- play2learn/settings.py
+++ play2learn/settings.py
@@ -40,12 +40,20 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    # Third-party
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # local apps
     'common.apps.CommonConfig',
     "games.apps.GamesConfig",
     'users.apps.UsersConfig',
 
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
